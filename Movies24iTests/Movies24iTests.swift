9f69d90eacc934272b98f7a96430696c47290784
Yasir M Turk <yasirmturk@gmail.com>
2019-04-17 15:46:03 +0500

    Initial Commit

---
+++ Movies24iTests/Movies24iTests.swift
@@ -0,0 +1,34 @@
+//
+//  Movies24iTests.swift
+//  Movies24iTests
+//
+//  Created by Yasir M Turk on 17/04/2019.
+//  Copyright © 2019 yasirmturk. All rights reserved.
+//
+
+import XCTest
+@testable import Movies24i
+
+class Movies24iTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
