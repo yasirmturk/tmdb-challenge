90a682f2ed097d50bf0389957dcf0166824b58ff
Yasir M Turk <yasirmturk@gmail.com>
2019-04-17 16:13:54 +0500

    project settings, adding MCoreKit and MUIKit

---
+++ MCoreKit/MCoreKit.h
@@ -0,0 +1,19 @@
+//
+//  MCoreKit.h
+//  MCoreKit
+//
+//  Created by Yasir M Turk on 17/04/2019.
+//  Copyright © 2019 yasirmturk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MCoreKit.
+FOUNDATION_EXPORT double MCoreKitVersionNumber;
+
+//! Project version string for MCoreKit.
+FOUNDATION_EXPORT const unsigned char MCoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCoreKit/PublicHeader.h>
+
+
---
+++ MUIKit/MUIKit.h
@@ -0,0 +1,19 @@
+//
+//  MUIKit.h
+//  MUIKit
+//
+//  Created by Yasir M Turk on 17/04/2019.
+//  Copyright © 2019 yasirmturk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MUIKit.
+FOUNDATION_EXPORT double MUIKitVersionNumber;
+
+//! Project version string for MUIKit.
+FOUNDATION_EXPORT const unsigned char MUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MUIKit/PublicHeader.h>
+
+
